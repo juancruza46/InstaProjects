2e9209bb5d4959a36d49f2c24615662a86ce5b73
juancruza46 <juan.cruza46@gmail.com>
2024-02-14 00:57:58 -0500

    create NavBar.css, style NavBar lightly

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,16 +1,31 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import './NavBar.css'; // Import your CSS file
 
-const NavBar = () => {
+const NavBar = ({ onLinkClick }) => {
+  const handleLinkClick = (route) => {
+    if (onLinkClick) {
+      onLinkClick(route);
+    }
+  };
+
   return (
-    <nav>
-      <Link to="/home">Home</Link>
-      &nbsp; | &nbsp;
-      <Link to="/favorites">Favorites</Link>
-      &nbsp; | &nbsp;
-      <Link to="/add-post">Add Post</Link>
-      &nbsp; | &nbsp;
-      <Link to="/category/react">Category</Link>
+    <nav className="navbar">
+      <Link to="/home" onClick={() => handleLinkClick('/home')}>
+        Home
+      </Link>
+      <span className="divider"> | </span>
+      <Link to="/favorites" onClick={() => handleLinkClick('/favorites')}>
+        Favorites
+      </Link>
+      <span className="divider"> | </span>
+      <Link to="/add-post" onClick={() => handleLinkClick('/add-post')}>
+        Add Post
+      </Link>
+      <span className="divider"> | </span>
+      <Link to="/category/react" onClick={() => handleLinkClick('/category/react')}>
+        Category
+      </Link>
     </nav>
   );
 };
